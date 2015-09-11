c4018b16dec3175729cb463457a3b3a3e56b91df
paroque28 <paroque28@users.noreply.github.com>
2015-09-11 17:00:16 -0600

    Update UserInterface.java

--- Program/src/com/celci2015/UserInterface.java
+++ Program/src/com/celci2015/UserInterface.java
@@ -8,14 +8,15 @@ import com.celci2015.math.MathExpression;
 public abstract class UserInterface {
 
     public static final String OPERATION = "\\ ([+*-]|[e])\\ ";
+    //Pattern this form : # # # | # # # | .... operation # # # | # # # .... where operation can be *,+,- or e.
     public static final String expressionPattern = "(-\\d+|\\d+)\\ (-\\d+|\\d+)\\ (-\\d+|\\d+)(\\ \\|\\ (-\\d+|\\d+)\\ (-\\d+|\\d+)\\ (-\\d+|\\d+))*\\ (([+*-]\\ (-\\d+|\\d+)\\ (-\\d+|\\d+)\\ (-\\d+|\\d+)(\\ \\|\\ (-\\d+|\\d+)\\ (-\\d+|\\d+)\\ (-\\d+|\\d+))*)|([e]\\ (-\\d+|\\d+)\\ (-\\d+|\\d+)))";
     public static final String BAD_INPUT = "Bad input!";
 
     public abstract void display(String result);
     public void query(String queryString)
     {
-        if (!queryString.matches(expressionPattern)) {
-            if (queryString.matches(Processor.expressionPattern))
+        if (!queryString.matches(expressionPattern)) {// Verify expression 
+            if (queryString.matches(Processor.expressionPattern))// verify is its a single expression
             {
                 display(queryString);
               return;
@@ -26,6 +27,7 @@ public abstract class UserInterface {
         String[] inputs = queryString.split(OPERATION);
         MathExpression expression;
         try{
+            //First we get the operation, then parse a math expression and then execute the other expression
         switch (queryString.charAt(inputs[0].length() + 1)) {// get operation
             case '*':
                 expression = new MathExpression(inputs[0]);
@@ -53,8 +55,8 @@ public abstract class UserInterface {
         }}
         catch(Exception e)
         {
-            e.printStackTrace();
-            display(BAD_INPUT +" "+ e.getMessage());
+            //e.printStackTrace();
+            display(BAD_INPUT +" "+ e.getMessage());// Display error messagee
 
         }
     }
